1e2a59120d8a5453f03aba53858f142acad73559
Oscar Eduardo Raygoza <oscar.eduardo.raygoza@gmail.com>
2022-03-23 13:41:05 -0500

    reduce limit search

--- src/apis/searchPlacesAPI.ts
+++ src/apis/searchPlacesAPI.ts
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const BASE_URL_MAP_API = "https://api.mapbox.com/geocoding/v5/mapbox.places";
 
-const searchPlaces = axios.create({
+const searchPlacesAPI = axios.create({
   baseURL: BASE_URL_MAP_API,
   params: {
     access_token:
       "pk.eyJ1Ijoib3NjYXJyYXlnb3phIiwiYSI6ImNsMHlkZGphbTF6dHUzY3Brdjh4bW80c3EifQ.129PXQKAXa2Ls5UFHH5Rog",
     language: "es",
-    limit: 5,
+    limit: 3,
   },
 });
 
-export default searchPlaces;
+export default searchPlacesAPI;
